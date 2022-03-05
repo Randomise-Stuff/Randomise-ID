d266a92db159d1097c257f879f996a6fd48dcaa2
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-03-05 23:13:12 +0530

    Initial Commit - Package Version v1.0.0

---
+++ README.md
@@ -0,0 +1,23 @@
+# Randomise ID
+
+randomise-id is a NPM package which generates random IDs for your coding projects.
+
+# How to install this package?
+
+```sh
+npm i randomise-id
+```
+
+# Examples
+
+.generate(length)
+
+```js
+const id = require("randomise-id");
+
+console.log(id.generate(10)); //returns a random ID with 10 characters
+```
+
+# Donate us to Improve
+
+Patreon - [Click Here](https://patreon.com/ashutoshswamy)
---
+++ functions/generate.js
@@ -0,0 +1,20 @@
+module.exports = (length) => {
+  Number(length);
+
+  if (!length) {
+    return new TypeError(
+      "Please enter the number of characters you want in this ID."
+    );
+  }
+
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
